d1aa9210fc18db1942d385fcce88d2bd6e8d4c09
KarlJay <KarlJay@email.com>
2019-09-28 08:49:06 -0700

    Simple test of the VC system in Xcode

--- VerControlTest/ViewController.swift
+++ VerControlTest/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        // this is where the button press goes
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var labelLink: UILabel!
+    
 }
 
