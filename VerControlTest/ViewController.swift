95c8e58e7513ba6cc58826f74d7d291d99c4ab07
KarlJay <KarlJay@email.com>
2019-09-28 08:43:30 -0700

    Initial Commit

---
+++ VerControlTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerControlTest
+//
+//  Created by Karl Jay on 9/28/19.
+//  Copyright © 2019 KarlJay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
